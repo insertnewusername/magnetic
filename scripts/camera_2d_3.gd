7fb4e0bf120d80c7756e7fb61a8a535b4de13031
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-09 12:54:35 +1000

    added area2d to make cameralimitchange
    Had issues last time where i adjusted camera2d3 limits, as it was just wrong. So i added a area2d, and made it signal to camera2d3 when player entered an area that needed the cameralimits to change, and added 2 functions to camera2d3 script, one for change, one for back to normal when player got back up again. Worked very well.

---
+++ scripts/camera_2d_3.gd
@@ -0,0 +1,26 @@
+extends Camera2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+
+func change() -> void:
+	limit_left = 1500
+	limit_top = 108
+	limit_right = 5500
+	limit_bottom = 1019
+	
+	print("cameraupdated")
+	
+func backtonormal() -> void:
+	limit_left = -96
+	limit_top = 0
+	limit_right = 7040
+	limit_bottom = 512
+	
+	print("cameraupdateagain")
+	
