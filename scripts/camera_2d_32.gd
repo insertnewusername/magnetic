c4da552d5261ee19abd36ba3d2e1ae3cc7b428a4
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-20 18:23:04 +1000

    fix camera and remove unneeded script

--- scripts/camera_2d_32.gd
+++ scripts/camera_2d_32.gd
@@ -6,6 +6,32 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+func change() -> void:
+	limit_left = 16383
+	limit_top = 391
+	limit_right = 18057
+	limit_bottom = 915
+	
+	print("cameraupdated")
+	
+func backtonormal() -> void:
+	limit_left = -96
+	limit_top = 0
+	limit_right = 21057
+	limit_bottom = 513
+	
+	print("cameraupdateagain")
+	
+func change2() -> void:
+	limit_left = 18245
+	limit_top = -561
+	limit_right = 20072
+	limit_bottom = 113
+	
+func change3() -> void:
+	limit_left = 16383
+	limit_top = 391
+	limit_right = 18057
+	limit_bottom = 915
