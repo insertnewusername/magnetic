a4160b96c6dec18f4412bfb6342464ca078668e4
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-20 13:17:43 +1000

    add home logic to level 1 and 2

--- scripts/level_2.gd
+++ scripts/level_2.gd
@@ -40,3 +40,7 @@ func _on_redo_pressed() -> void:
 func _on_bgmusic_finished() -> void:
 	print("finished")
 	bgmusic.play()
+
+
+func _on_texture_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://sprites/levels.tscn")
